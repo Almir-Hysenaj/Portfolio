29ea2bbde4332ac53efd21d1cfa234cf39712ab8
Almir Hysenaj <almir_hysenaj@outlook.com>
2026-02-06 19:34:48 +0000

    fixed?

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-7 left-1/2 -translate-x-1/2 px-16 rounded-full overflow-hidden border-teal-800 border-[0.5px] bg-gray-950/50 backdrop-blur-sm shadow-md z-50 w-[90vw] sm:w-auto">
+    <nav className="fixed top-7 left-1/2 -translate-x-1/2 px-4 sm:px-16 rounded-full overflow-hidden border-teal-800 border-[0.5px] bg-gray-950/50 backdrop-blur-sm shadow-md z-50 w-[90vw] sm:w-auto">
       <div className="max-w-7xl mx-auto py-4 flex justify-center">
         <div className="space-x-10 sm:space-x-14 md:space-x-20 flex">
           {links.map((link, idx) => (
