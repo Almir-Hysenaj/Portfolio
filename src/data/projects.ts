995e90baecbbe7526138729460e026a236844c9d
Almir Hysenaj <almir_hysenaj@outlook.com>
2026-02-14 17:52:58 +0000

    add wordle project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,12 +1,21 @@
+import WordleImage from '../assets/projects/wordle.png';
 import WeatherAppImage from '../assets/projects/weather-app.png';
 import ToDoListImage from '../assets/projects/todo-list.png';
 
 export const projects = [
   {
+    title: 'Wordle Clone',
+    description: 'A recreation of the popular game Wordle',
+    tech: ['React', 'API', 'TypeScript', 'Tailwind'],
+    github: 'https://github.com/Almir-Hysenaj/Wordle',
+    demo: 'https://almir-hysenaj.github.io/Wordle/',
+    image: WordleImage,
+  },
+  {
     title: 'Weather App',
     description:
       'A web app that displays current weather information for any location.',
-    tech: ['React', 'TypeScript', 'Tailwind'],
+    tech: ['React', 'API', 'TypeScript', 'Tailwind'],
     github: 'https://github.com/Almir-Hysenaj/Weather-App',
     demo: 'https://almir-hysenaj.github.io/Weather-App/',
     image: WeatherAppImage,
